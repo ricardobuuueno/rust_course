c8ca0255309da942f12781e12d23702f6f7c9c1d
Ricardo Bueno <ricardo.bueno.sf.dev@hotmail.com>
2022-09-28 18:38:59 -0300

    Section 8 - Conclusion

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,24 @@ struct GroceryItem {
     price: f64
 }
 
+fn clamp(n: i32, lower: i32, upper: i32) -> i32 {
+    if n < lower {
+        lower
+    } else if n > upper {
+        upper
+    } else {
+        n
+    }
+}
+
+fn div(a: i32, b: i32) -> Option<i32> {
+    Some(a / b)
+}
+
+fn concat(first: &str, second: &str) -> String {
+    format!("{}{}", first, second)
+}
+
 fn main() {
     println!("Hello, world!");
 
@@ -40,4 +58,37 @@ fn main() {
     let user_info = ("Emma", 20);
     println!("{:?}, {:?}", user_info.0, user_info.1);
 
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn clamp_lower() {
+        let result = clamp(10, 100, 1000);
+        let expected = 100;
+        assert_eq!(result, expected, "should be 100");
+    }
+
+    #[test]
+    fn clamp_upper() {
+        let result = clamp(5000, 100, 1000);
+        let expected = 1000;
+        assert_eq!(result, expected, "should be 1000");
+    }
+
+    #[test]
+    fn check_div() {
+        let result = div(1,1);
+        let expected = Some(1);
+        assert_eq!(result, expected, "should be 1");
+    }
+
+    #[test]
+    fn check_concat() {
+        let result = concat("a", "b");
+        let expected = String::from("ab");
+        assert_eq!(result, expected, "Should be equal");
+    }
 }
--- src/p1.rs
+++ src/p1.rs
@@ -1,56 +1,130 @@
 use std::io;
+use std::collections::HashMap;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Bill {
     name: String,
     amount: f64,
 }
 
 struct Bills {
-    inner: Vec<Bill>,
+    inner: HashMap<String,Bill>,
 }
 
 impl Bills {
     fn new() -> Self {
-        Self { inner: vec![] }
+        Self { inner: HashMap::new(), }
     }
 
     fn add(&mut self, bill: Bill) {
-        self.inner.push(bill);
+        self.inner.insert(bill.name.clone(), bill);
     }
 
-    fn get_all(&self) -> &Vec<Bill> {
-        &self.inner
+    fn get_all(&self) -> Vec<Bill> {
+        let mut bills = vec![];
+        for bill in self.inner.values() {
+            bills.push(bill.clone());
+        }
+        bills
     }
+
+    fn remove(&mut self, name: &str) -> bool {
+        self.inner.remove(name).is_some()
+    }
+
+    fn update(&mut self, name: &str, amount: f64) -> bool {
+        match self.inner.get_mut(name) {
+            Some(bill) => {
+                bill.amount = amount;
+                true
+            }
+            Nome => false
+        }
+    }
+    
 }
 
-fn get_input() -> String {
+fn get_input() -> Option<String> {
     let mut buffer = String::new();
     while io::stdin().read_line(&mut buffer).is_err() {
         println!("Please enter your data again");
     }
-    buffer.trim().to_owned()
+    let input = buffer.trim().to_owned();
+    if &input == "" {
+        None
+    } else {
+        Some(input)
+    }
 }
 
-fn get_bill_amount() -> f64 {
+fn get_bill_amount() -> Option<f64> {
     println!("Amount:");
     loop {
-        let input: String = get_input();
+        let input = match get_input() {
+            Some(input) => input,
+            None => return None,
+        };
+        if &input == "" {
+            return None;
+        }
+
         let parsed_input: Result<f64, _> = input.parse();
         match parsed_input {
-            Ok(amount) => return amount,
+            Ok(amount) => return Some(amount),
             Err(_) => println!("Please enter a number"),
         }
     }
 }
 
 fn add_bill_menu(bills: &mut Bills) {
-    println!("Bill name:");1
-    let name = get_input();
-    let amount = get_bill_amount();
+    println!("Bill name:");
+    let name = match get_input() {
+        Some(input) => input,
+        None => return,
+    };
+    let amount = match get_bill_amount() {
+        Some(amount) => amount,
+        None => return,
+    };
     let bill = Bill { name, amount };
     bills.add(bill);
     println!("Bill added");
+}
+
+fn remove_bill_menu(bills: &mut Bills) {
+    for bill in bills.get_all() {
+        println!("{:?}", bill);
+    }
+    println!("Enter bill name to remove:");
+    let input = match get_input() {
+        Some(input) => input,
+        None => return,
+    };
+    if bills.remove(&input) {
+        println!("removed");
+    } else {
+        println!("bill not found");
+    }
+}
+
+fn update_bill_menu(bills: &mut Bills) {
+    for bill in bills.get_all() {
+        println!("{:?}", bill);
+    }
+    println!("Enter bill name to update:");
+    let name = match get_input() {
+        Some(name) => name,
+        None => return,
+    };
+    let amount = match get_bill_amount() {
+        Some(amount) => amount,
+        None => return,
+    };
+    if bills.update(&name, amount) {
+        println!("updated");
+    } else {
+        println!("bill not found");
+    }
 }
 
 fn view_bill_menu(bills: &Bills) {
@@ -65,6 +139,8 @@ fn main_menu() {
         println!("== Manage Bills ==");
         println!("1. Add bill");
         println!("2. View bill");
+        println!("3. Remove bill");
+        println!("4. Update bill");
         println!("");
         println!("Enter selection:");
     }
@@ -73,10 +149,15 @@ fn main_menu() {
 
     loop {
         show();
-        let input = get_input();
+        let input = match get_input() {
+            Some(input) => input,
+            None => return,
+        };
         match input.as_str() {
             "1" => add_bill_menu(&mut bills),
             "2" => view_bill_menu(&bills),
+            "3" => remove_bill_menu(&mut bills),
+            "4" => update_bill_menu(&mut bills),
             _ => break,
         }
     }
